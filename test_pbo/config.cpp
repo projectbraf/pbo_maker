0038c71d5fc276f6ca94a5fbf37983dcd9848dbd
Valmo Trindade <val_mo_jr@hotmail.com>
2024-11-02 21:34:24 -0300

    added test_pbo to test the pbo maker

---
+++ test_pbo/config.cpp
@@ -0,0 +1,12 @@
+class CfgPatches {
+  class test_main {
+    units[] = {""};
+    weapons[] = {""};
+    author = "Valmo";
+    url = "";
+    requiredAddons[] = {
+      "A3_Data_F"
+    };
+    requiredVersion = 0.1;
+  };
+};
