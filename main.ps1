4924c00ee8bda76563947affb5f9a14a8f5584c7
Valmo Trindade <val_mo_jr@hotmail.com>
2024-11-04 17:22:06 -0300

    testing changed folder filters and addonprefix

--- main.ps1
+++ main.ps1
@@ -4,4 +4,23 @@ Write-Output "After Event Commit -> $afterEventCommit"
 Write-Output "Build Tool -> $build_tool"
 Write-Output "Private Key -> $private_key"
 
-python make.py $beforeEventCommit $afterEventCommit $build_tool $private_key
+$changedFiles = git diff --name-only $beforeCommit $afterCommit
+Write-Output "Changed Files: $changedFiles"
+$changedDirs = $changedFiles | ForEach-Object { $_.Split('/')[0] } | Select-Object -Unique
+Write-Output "Changed Folders: $changedDirs"
+
+$symlinkPath = "P:\$projectName"
+
+if (Test-Path $symlinkPath -PathType Leaf -or Test-Path $symlinkPath -PathType Container) {
+    Remove-Item $symlinkPath -Force
+}
+
+New-Item -ItemType SymbolicLink -Path "$symlinkPath" -Target .
+
+$changedDirs | ForEach-Object {
+  $folder = $_
+  if ($folder -like "$addonPrefix*") {
+    $packedDir = "$symlinkPath\${folder}"
+    Write-Output "Packing Folder: $packedDir"
+  }
+};
